7d7a8654a11e31ea35ec2500c858a5b8df52d85c
hatimrustam32 <rustamhatim32@gmail.com>
2024-06-13 16:46:47 +0530

    code change

--- Add Modify Delete Users/admin/update_post_code.php
+++ Add Modify Delete Users/admin/update_post_code.php
@@ -1,42 +1,55 @@
 <?php
-    session_start();
-    if(!isset($_SESSION["user_name"])){
-        header("location:http://localhost/Add%20Modify%20Delete%20Users/admin/admin/index.php");
-    }
+session_start();
+if(!isset($_SESSION["user_name"])){
+    header("location:http://localhost/Add%20Modify%20Delete%20Users/admin/admin/index.php");
+    exit;
+}
 ?>
+
 <?php
-    include "configure.php";
-    if(empty($_FILES["post_img"]["name"])){
-        $filename = $_POST["old_img"];
-    }else{
-        $error = array();
-        $filename = $_FILES["post_img"]["name"];
-        $filesize = $_FILES["post_img"]["size"];
-        $filetmp = $_FILES["post_img"]["tmp_name"];
-        $filetype = $_FILES["post_img"]["type"];
-        $fileext = end(explode('.',$filename));
-        $extension = array("jpeg","png","jpg");
-        if(in_array($fileext,$extension)===false){
-            $error[]="this extension is not allow plz use this formate jpeg,png,jpg";
-        }
-        if($filesize>2000000){
-            $error[]="file size must in 2mb";
-        }
-        if(empty($error)==true){
-            move_uploaded_file($filetmp,"upload/".$filename);
-        }
-        else{
-            print_r($error);
-            die();
-        }
+include "configure.php";
+
+if(empty($_FILES["post_img"]["name"])){
+    $filename = $_POST["old_img"];
+}else{
+    $error = array();
+    $filename = $_FILES["post_img"]["name"];
+    $filesize = $_FILES["post_img"]["size"];
+    $filetmp = $_FILES["post_img"]["tmp_name"];
+    $filetype = $_FILES["post_img"]["type"];
+    $fileext = end(explode('.',$filename));
+    $extension = array("jpeg","png","jpg");
+    if(in_array($fileext,$extension) === false){
+        $error[] = "This extension is not allowed, please use the format jpeg, png, jpg.";
     }
-    $querry = "UPDATE post set title = '{$_POST["title"]}', description = '{$_POST["description"]}', category = {$_POST["category"]}, post_img = '{$filename}' WHERE post_id = {$_POST["post_id"]};";
-    if($_POST["old_category"] != $_POST["category"]){
-        $querry .= "update category set post = post - 1 where category_id = {$_POST['old_category']};";
-        $querry .= "update category set post = post + 1 where category_id = {$_POST["category"]};";
+    if($filesize > 2000000){
+        $error[] = "File size must be within 2MB.";
     }
-    $sql = mysqli_multi_query($connection,$querry) or die("query fail");
-    if($sql){
-        header("location:http://localhost/Add%20Modify%20Delete%20Users/admin/post.php");
+    if(empty($error) == true){
+        move_uploaded_file($filetmp, "upload/" . $filename);
+    } else {
+        print_r($error);
+        die();
     }
+}
+
+$title = mysqli_real_escape_string($connection, $_POST["title"]);
+$description = mysqli_real_escape_string($connection, $_POST["description"]);
+$category = mysqli_real_escape_string($connection, $_POST["category"]);
+$post_id = mysqli_real_escape_string($connection, $_POST["post_id"]);
+$old_category = mysqli_real_escape_string($connection, $_POST["old_category"]);
+
+$query = "UPDATE post SET title = '{$title}', description = '{$description}', category = {$category}, post_img = '{$filename}' WHERE post_id = {$post_id};";
+
+if($old_category != $category){
+    $query .= "UPDATE category SET post = post - 1 WHERE category_id = {$old_category};";
+    $query .= "UPDATE category SET post = post + 1 WHERE category_id = {$category};";
+}
+
+$sql = mysqli_multi_query($connection, $query) or die("Query failed: " . mysqli_error($connection));
+
+if($sql){
+    header("location:http://localhost/Add%20Modify%20Delete%20Users/admin/post.php");
+    exit;
+}
 ?>
