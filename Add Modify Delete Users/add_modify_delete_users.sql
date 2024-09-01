d1ddb9a35f8af7d437267e24005f5214e056b178
hatimrustam32 <rustamhatim32@gmail.com>
2024-09-02 01:56:15 +0530

    sql

--- Add Modify Delete Users/add_modify_delete_users.sql
+++ Add Modify Delete Users/add_modify_delete_users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 13, 2024 at 09:02 AM
+-- Generation Time: Sep 01, 2024 at 10:16 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -38,10 +38,9 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`category_id`, `category_name`, `post`) VALUES
-(1, 'Monsoon Season', 0),
-(2, 'Education Sector', 1),
-(3, 'Tourism', 1),
-(4, 'Health Education', 1);
+(1, 'India-China relations', 1),
+(2, 'Space exploration', 1),
+(3, 'Social movements', 0);
 
 -- --------------------------------------------------------
 
@@ -52,7 +51,7 @@ INSERT INTO `category` (`category_id`, `category_name`, `post`) VALUES
 CREATE TABLE `post` (
   `post_id` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
-  `description` varchar(255) NOT NULL,
+  `description` text NOT NULL,
   `category` varchar(50) NOT NULL,
   `post_date` varchar(100) NOT NULL,
   `author` int(11) NOT NULL,
@@ -64,9 +63,8 @@ CREATE TABLE `post` (
 --
 
 INSERT INTO `post` (`post_id`, `title`, `description`, `category`, `post_date`, `author`, `post_img`) VALUES
-(1, 'Innovative Health Education Programs Transforming School Curriculums', 'Schools across the country are integrating innovative health education programs into their curriculums to promote wellness and healthy living among students. These programs focus on mental health, nutrition, and physical activity, aiming to create a holis', '4', '13-Jun-2024', 2, 'what-is-health-education-definition-topics-role-in-society_165447.jpg'),
-(2, 'Tourism Industry Rebounds with New Sustainable Travel Options', 'As the tourism industry recovers from the pandemic, new sustainable travel options are gaining popularity among eco-conscious travelers. Destinations worldwide are adopting green practices to attract tourists while preserving natural resources and reducin', '3', '13-Jun-2024', 1, 'tourismxi-171103130015-thumbnail.jpg'),
-(3, 'Tech Advancements Revolutionize the Education Sector', 'Technological advancements are revolutionizing the education sector, offering new learning methods and tools that enhance student engagement and accessibility. From virtual classrooms to AI-driven personalized learning, education is becoming more adaptive', '2', '13-Jun-2024', 2, '1690908658-education sector in india.jpg');
+(1, 'India-China Relations 2024: Border Tensions, Diplomatic Dialogues, and Strategic Maneuvers in a Chan', 'In 2024, India-China relations remain tense as both nations navigate ongoing border disputes and strategic challenges. The Line of Actual Control (LAC) continues to be a flashpoint, with military standoffs and infrastructure developments heightening tensions. Diplomatic efforts, including high-level talks and back-channel negotiations, aim to de-escalate the situation, but mutual distrust persists. Both countries are also expanding their regional influence, with India strengthening ties with the Quad and China advancing its Belt and Road Initiative. The complex interplay of rivalry and cooperation will define the bilateral relationship in 2024, with significant implications for regional stability.', '1', '01-Sep-2024', 1, '1914b6b7-726b-4e53-bda8-ebaf9a9cb8c1.webp'),
+(2, 'India\'s Space Exploration in 2024: Ambitious Missions, Lunar Landings, and Expanding Frontiers in Sp', 'In 2024, India\'s space exploration efforts have reached new heights with ambitious missions aimed at deepening the country’s presence in space. The Indian Space Research Organisation (ISRO) successfully launched several missions, including lunar exploration and satellite deployments, showcasing India\'s growing capabilities in space technology. Key milestones include the Chandrayaan-4 mission, aiming to land on the moon\'s south pole, and the development of advanced satellites for communication and earth observation. Collaborative efforts with global space agencies are also on the rise, positioning India as a significant player in the global space race. The year marks a pivotal moment in India\'s quest to explore the cosmos.', '2', '01-Sep-2024', 1, 'efe77e52-ac6d-465e-8fb0-ba9c440a52f6.webp');
 
 -- --------------------------------------------------------
 
@@ -88,8 +86,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `first_name`, `last_name`, `user_name`, `password`, `role`) VALUES
-(1, 'Hatim', 'Rustam', 'hatim_Rustam', '$2y$10$Zx0LnJzC0Oq5pQ65LZ6M6emySmWZvEdJIR7kOL3p486flFY1QEM5W', 2),
-(2, 'hatim', 'user', 'hatim_user', '$2y$10$CRzjDoUfNdueMhj3L0E51uY4A/9paM9KaRLPFWsw.tCHAb6p2WFka', 1);
+(1, 'Hatim', 'Admin', 'hatim_rustam', '$2y$10$LpKdVub3xfLJxdvUDTJu7.QhwHudlhXFa4LSGfxUWAbIEP2/NcRRW', 2),
+(2, 'hatim', 'User', 'hatim_user', '$2y$10$AWRCb6vLudTb1EscRnyICeuMMcKp6yUP679rNfqGIuyruhOuBBgN2', 1);
 
 --
 -- Indexes for dumped tables
@@ -121,13 +119,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user`
